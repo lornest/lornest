9b4e33884cec6941a7e13daba825862044ad6852
Lorne Stalker <lornest@users.noreply.github.com>
2024-03-28 14:48:42 +0000

    gke

---
+++ infra/gke.tf
@@ -0,0 +1,20 @@
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+# GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.gcp_region
+ 
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+ 
+  enable_autopilot = true
+}
---
+++ infra/variables.tf
@@ -0,0 +1,10 @@
+variable "gcp_project_id" {
+  type        = string
+  description = "GCP Project ID"
+}
+
+variable "gcp_region" {
+  type        = string
+  description = "GCP Region"
+  default     = "europe-west2"
+}
